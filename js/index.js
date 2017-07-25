71210b7a7a66ccc67e8a7b33bd07df08e532271b
Marilzon <maxmaril@hotmail.com>
2017-07-25 00:14:39 -0300

    codigo dominuido ttest

--- js/index.js
+++ js/index.js
@@ -9,30 +9,3 @@ jQuery(document).ready(function($) {
              $("#nav-links").toggle('slow');
          });
 //
-$(document).ready(function() {
-    // Main variables
-    var $aboutTitle = $('.about-myself .content h2');
-    var $developmentWrapper = $('.development-wrapper');
-    var developmentIsVisible = false;
-
-
-    /* ####### HERO SECTION ####### */
-
-    $('.hero .content .header').delay(500).animate({
-        'opacity': '1',
-        'top': '50%'
-    }, 1000);
-
-
-    $(window).scroll(function() {
-
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-        /* ##### ABOUT MYSELF SECTION #### */
-        if (bottom_of_window > ($aboutTitle.offset().top + $aboutTitle.outerHeight())) {
-            $('.about-myself .content h2').addClass('aboutTitleVisible');
-        }
-
-
-    }); // -- End window scroll --
-});
