da4871bf2b8016dc3a7747ad1393b604976d9d39
Marilzon <maxmaril@hotmail.com>
2020-07-27 13:32:04 -0300

    Adicionando Background Linear aos elementos

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 - [X] GITHUB-PAGES: (https://marilzon.github.io/maril-dev/)
 
 - Este é meu site, onde resumo de forma rapida minhas competencias e meus objetivos,
-um site simples, leve e com uma boa experiência de navegação.
+inspirado em estilos retro.
--- js/index.js
+++ js/index.js
@@ -16,11 +16,6 @@ let profile = {
     linkedin: "https://www.linkedin.com/in/marilzon",
     github: "https://github.com/Marilzon"
   },
-  jobs: [
-    "06/04/2020 - Atualmente, Operador de Telesserviços, Paschoalotto Serviços Financeiros.",
-    "01/01/2018 - 03/04/2020: Técnico de Informática freelancer.",
-    "25/02/2016 - 04/07/2017: Promotor de Vendas, RTM TASSE ASSESSORIA DE MERCADO LTDA."
-  ],
   skills: [
     "HTML",
     "CSS",
@@ -36,35 +31,39 @@ function addElement(tag, content) {
 }
 //addElement, TAG - CONTEUDO
 addElement('h1', 'Maril - Desenvolvedor Front-end')
-addElement('p', `
-  Olá, sou <em>${profile.name}</em> moro atualmente em ${profile.city}, tenho ${profile.age} anos
-  e estou em busca de oportunidades como <strong>Desenvolvedor Front-end</strong>`)
-addElement('p', `Telefone - Whats: ${profile.phone}`)
-addElement('p', `E-Mail: <u>${profile.email}</u>`)
-addElement('ul', `Redes sociais:
+
+addElement('h3', `
+  Olá, sou <u>${profile.name}</u> moro atualmente em ${profile.city}, tenho ${profile.age} anos
+  e estou em busca de oportunidades como <strong>Desenvolvedor Front-end</strong><hr>
+`)
+
+addElement('p', `<strong>Telefone - Whats</strong>: ${profile.phone}`)
+
+addElement('p', `<strong>E-Mail:</strong> <u>${profile.email}</u>`)
+
+addElement('ul', `<h3>Redes sociais:</h3>
   <li>
-    <a target='_blank' href='https://www.linkedin.com/in/marilzon'>
-      ${profile.social.linkedin}
+    <a target='_blank' href='${profile.social.linkedin}'>
+      LINKEDIN
     </a>
-  </li>
-  <li>
-    <a target='_blank' href='https://github.com/Marilzon'>
-      ${profile.social.github}
+      -
+    <a target='_blank' href='${profile.social.github}'>
+      GITHUB
     </a>
   </li>
 `)
-addElement('p', `Formação acadêmica: ${profile.education}`)
-addElement('ul', `Cursos:
+
+addElement('ul', `<h3>Formação acadêmica:</h3>
+  <li>${profile.education}</li>
+`)
+
+addElement('ul', `<h3>Cursos:</h3>
   <li>${profile.courses[0]}</li>
   <li>${profile.courses[1]}</li>
 `)
-addElement('ul', `Skills:
+
+addElement('ul', `<h3>Skills:</h3>
   <li>${profile.skills[0]}</li>
   <li>${profile.skills[1]}</li>
   <li>${profile.skills[2]}</li>
-`)
-addElement('ul', `Ultimas experiências profissionais:
-  <li>${profile.jobs[0]}</li>
-  <li>${profile.jobs[1]}</li>
-  <li>${profile.jobs[2]}</li>
 `)
