e63c22fdc4c9e620b043aad096aaed83848486e2
Marilzon <maxmaril@hotmail.com>
2020-09-18 10:59:23 -0300

    Docs: Atualizando texto de apresentacao

--- js/index.js
+++ js/index.js
@@ -50,7 +50,7 @@ const title = 'Maril - Desenvolvedor Front End'
 
 const myResume = `
   Olá, sou <u>${profile.name}</u> moro atualmente em ${profile.city}, tenho ${profile.age} anos
-  e estou em busca de oportunidades como <strong>Desenvolvedor Front-end</strong>
+  e estou em busca de oportunidades como <strong>Desenvolvedor WEB</strong>
 `
 const contact = `
   <h3>Contato:</h3>
