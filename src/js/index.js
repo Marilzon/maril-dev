3acfba4ab77c0bd1204eccb465c361a248e5af24
Marilzon <maxmaril@hotmail.com>
2022-09-11 21:28:02 -0300

    FEATURE: theme handler

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
-﻿const root = document.getElementById('#root');
+﻿const button = document.querySelector(".theme-btn")
+const currentTheme = document.querySelector("#theme-style")
+
+const dayIcon = document.querySelector(".day-icon")
+const nightIcon = document.querySelector(".night-icon")
+
+button.addEventListener("click", function() {
+    if (currentTheme.getAttribute("href") == "src/css/light-theme.css") {
+        currentTheme.href = "src/css/dark-theme.css"
+
+    } else {
+        currentTheme.href = "src/css/light-theme.css"
+    }
+})
