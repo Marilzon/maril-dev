45044a3014dc8225192d5550e91e41b7ba178a4d
Marilzon <maxmaril@hotmail.com>
2022-10-21 23:21:59 -0300

    UPDATE: Refactiring style rules

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,1 @@
-﻿const button = document.querySelector(".theme-btn")
-const currentTheme = document.querySelector("#theme-style")
-
-const dayIcon = document.querySelector(".day-icon")
-const nightIcon = document.querySelector(".night-icon")
-
-button.addEventListener("click", function() {
-    if (currentTheme.getAttribute("href") == "src/css/light-theme.css") {
-        currentTheme.href = "src/css/dark-theme.css"
-
-    } else {
-        currentTheme.href = "src/css/light-theme.css"
-    }
-})
+﻿
