588539ba76a7c88cf08d10b02f5c3e9037a1069f
Marilzon <maxmaril@hotmail.com>
2023-05-27 16:54:40 -0300

    feature: images interval

--- src/js/background.js
+++ src/js/background.js
@@ -1,28 +1,20 @@
-const MAX_IMAGE_NUMBER = 10;
+const MAX_IMAGE_NUMBER = 13;
 const IMAGE_PATH = "../../assets/images/";
 
 document.addEventListener("DOMContentLoaded", () => {
+  setBackground();
+
   setInterval(setBackground, 6000);
 });
 
 function setBackground() {
   const introScreen = document.getElementById("screen__intro");
-
   const imageNumber = Math.floor(Math.random() * MAX_IMAGE_NUMBER) + 1;
+  introScreen.style.backgroundImage = `url("${IMAGE_PATH}${imageNumber}-min.gif")`;
 
   if (introScreen) {
-    introScreen.classList.add("fade-out");
-
-    setTimeout(() => {
-      introScreen.style.backgroundImage = `url("${IMAGE_PATH}${imageNumber}.gif")`;
-      introScreen.classList.remove("fade-out");
-      introScreen.classList.add("fade-in");
-
-      setTimeout(() => {
-        introScreen.classList.remove("fade-in");
-      }, 2000);
-    }, 2000);
+    return introScreen.classList.add("fade-out");
   }
-}
 
-setBackground();
+  return;
+}
