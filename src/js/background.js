a43e4e3a3bc9e221e3b3187c6b2ead96acbdcb27
Marilzon <maxmaril@hotmail.com>
2023-05-29 11:21:23 -0300

    fix: fixing background.js

--- src/js/background.js
+++ src/js/background.js
@@ -8,14 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
   setInterval(setBackground, 6000);
 });
 
-function setBackground() {
+function setBackground () {
   const introScreen = document.getElementById("screen__intro");
   const imageNumber = Math.floor(Math.random() * MAX_IMAGE_NUMBER) + 1;
-  introScreen.style.backgroundImage = `url("${IMAGE_PATH}${imageNumber}-min.gif")`;
-
-  if (introScreen) {
-    return introScreen.classList.add("fade-out");
-  }
-
-  return;
+  return introScreen.style.backgroundImage = `url("${IMAGE_PATH}${imageNumber}-min.gif")`;
 }
