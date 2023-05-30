1adb246a6cca13d56b59f15f8847620ddea62ef6
Marilzon <maxmaril@hotmail.com>
2023-05-30 15:07:43 -0300

    delete: reducing wheight of project'

--- src/js/background.js
+++ src/js/background.js
@@ -1,15 +1,23 @@
-const MAX_IMAGE_NUMBER = 9;
+const MAX_IMAGE_NUMBER = 4;
 const IMAGE_PATH =
   "https://cdn.jsdelivr.net/gh/Marilzon/maril-dev@master/src/assets/images/";
+
+const images = [];
+
+for (let i = 1; i <= MAX_IMAGE_NUMBER; i++) {
+  const img = new Image();
+  img.src = `${IMAGE_PATH}${i}-min.gif`;
+  images.push(img);
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   setBackground();
 
-  setInterval(setBackground, 6000);
+  setInterval(setBackground, 8000);
 });
 
 function setBackground () {
   const introScreen = document.getElementById("screen__intro");
-  const imageNumber = Math.floor(Math.random() * MAX_IMAGE_NUMBER) + 1;
-  return introScreen.style.backgroundImage = `url("${IMAGE_PATH}${imageNumber}-min.gif")`;
+  const imageNumber = Math.floor(Math.random() * MAX_IMAGE_NUMBER);
+  introScreen.style.backgroundImage = `url("${images[imageNumber].src}")`;
 }
